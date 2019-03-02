59ca1c64889792b2332367cbe53ceea45866c88a
Kalkidan Lemma <k.lemma14@gmail.com>
2019-03-02 17:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function logShout(string) {
 }
 function logWhisper(string) {
     console.log('hello'.toLowerCase())
-}
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase()
-  return "I can't hear you!".toLowerCase()
-  
-}
-function sayHiToGrandma(string) {
-  return "YES INDEED!"
-}
-function sayHiToGrandma(string) {
-  return "I love you, too."
-}
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+ }
